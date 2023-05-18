bd368eff5692a2f97eecd058905bb5a7bd998ef7
David Maman <david@zo.me>
2023-05-18 15:39:34 -0400

    use file instead

--- src/proxy-multi.ts
+++ src/proxy-multi.ts
@@ -1,16 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { Env } from './types/Env';
 import { proxyRequest } from './common';
+import services from './config/services.json';
 
 dotenv.config();
-
-const baseUrls = {
-    zo_eth: process.env.SERVICE1_BASE_URL,
-    zo_sol: process.env.SERVICE2_BASE_URL,
-    // Add more services as required...
-};
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -20,7 +14,7 @@ let corsOptions = {
 };
 app.use(cors(corsOptions));
 app.use(express.json());
-for (const [service, baseUrl] of Object.entries(baseUrls)) {
+for (const [service, baseUrl] of Object.entries(services)) {
     if (!baseUrl) {
         console.error(`Base URL for service ${service} not defined`);
         continue;
